8e51187c64b5cbb4e3bf992cb589e8f5679fe8dd
Daniel <dansan1234567@users.noreply.github.com>
2021-03-30 20:59:32 -0400

    Subiendo archivos de prática

---
+++ practical_assignment1/js/script.js
@@ -0,0 +1,9 @@
+(function() {
+    var app = angular.module('AttractionsModule', []);
+    var controller = app.controller('AttractionsController', ($scope) => {
+        /** TO DO
+            Complete all required code to complete the functionality of the application
+        */
+    });
+
+}());
